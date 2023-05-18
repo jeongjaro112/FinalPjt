e40388bdd7c114f62cc6381d9a66ae47858bf876
jj112 <kjy153@naver.com>
2023-05-18 10:53:54 +0900

    c_kjw_230518 ignitation commit

---
+++ fpjt/fpjt/spiders/stc.py
@@ -0,0 +1,49 @@
+import requests
+import pandas as pd
+import os
+import datetime
+from bs4 import BeautifulSoup
+
+print(f'\n\n########## 시   작 ##########\n\n')
+
+url_stockCode = 'https://kind.krx.co.kr/corpgeneral/corpList.do'
+
+
+params_stockCode = {
+    'method':'download',
+    'pageIndex':1,
+    'currentPageSize':3000,
+    'comAbbrvTmp':'',
+    'beginIndex':'',
+    'orderMode':3,
+    'orderStat':'D',
+    'isurCd':'',
+    'replsuSrtCd':'',
+    'searchType':'',
+    'marketType':'',
+    'searchType':13,
+    'industry':'',
+    'fiscalYearEnc':'all',
+    'comAbbrvTmp':'',
+    'location':'all'
+}    
+
+
+headers ={
+    'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Whale/3.20.182.14 Safari/537.36'
+}
+
+rs_stockCode = requests.post(url_stockCode, data=params_stockCode, headers=headers)
+
+if rs_stockCode.status_code != 200:
+    print(f'\n☞ status_stockCode Err :  {rs_stockCode.status_code}')
+else:
+    print(f'\n☞ successful requests & status_stockCode  :  {rs_stockCode.status_code}')
+    df_code = pd.read_html(rs_stockCode.text, converters={'종목코드': str})[0]
+    df_code['종목코드'] = '"'+ df_code['종목코드'] + '"' #종목코드 숫자앞 0이 유지되면서 to_csv()로 출력되도록 한 코딩임
+    stamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    file_name = f'stockCode_{stamp}.csv'
+    file_path = os.path.join(os.getcwd(), 'fhs', file_name)
+    df_code.to_csv(file_path, index=False)
+    print(f'\n\n########## 종   료 ##########\n\n')
+      
---
+++ fpjt/fpjt/spiders/sti.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import scrapy
+import os
+import glob
+from bs4 import BeautifulSoup
+#from items import FpjtItem
+
+class StiSpider(scrapy.Spider):
+    name = "sti"
+    
+    def start_requests(self):
+        file_path = os.path.join(os.getcwd(), 'fhs', 'stockCodeTest.csv')
+        stcd = pd.read_csv(file_path)
+        stcd['종목코드'] = stcd['종목코드'].str.strip('""')
+        for code in stcd['종목코드']:
+            url=f'https://navercomp.wisereport.co.kr/v2/company/ajax/cF1001.aspx?cmp_cd={code}'
+            params_sti = {
+                'fin_typ': 0,
+                'freq_typ': 'A',
+                'encparam': 'Zm9VajB4SlZDdlRjWFljOXRqMm0zdz09',
+                'id': 'ZTRzQlVCd0'
+                }
+            headers = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Whale/3.20.182.14 Safari/537.36'
+            }
+            scr = scrapy.Request(url, callback=self.parse, meta=params_sti, headers=headers)
+            yield scr
+    def parse(self, rs):
+        sp = BeautifulSoup(rs.text, 'html.parser')
+        print(sp)
+            
+        
+        
+            
+    
+
